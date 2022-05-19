9a4fac1cb5175526919604a534af001d2375dfdb
DevExpressExampleBot <fakeEmail>
2022-05-19 12:31:35 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,69 +1,68 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace Q184421
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.comboBox1 = New System.Windows.Forms.ComboBox()
             Me.label1 = New System.Windows.Forms.Label()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
-            Me.pivotGridControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
             Me.pivotGridControl1.Location = New System.Drawing.Point(13, 40)
             Me.pivotGridControl1.Name = "pivotGridControl1"
             Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
             Me.pivotGridControl1.Size = New System.Drawing.Size(718, 449)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'comboBox1
-            '
+            ' 
+            ' comboBox1
+            ' 
             Me.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
             Me.comboBox1.FormattingEnabled = True
             Me.comboBox1.Location = New System.Drawing.Point(106, 10)
             Me.comboBox1.Name = "comboBox1"
             Me.comboBox1.Size = New System.Drawing.Size(121, 21)
             Me.comboBox1.TabIndex = 1
-            '
-            'label1
-            '
+            AddHandler Me.comboBox1.SelectedIndexChanged, New System.EventHandler(AddressOf Me.comboBox1_SelectedIndexChanged)
+            ' 
+            ' label1
+            ' 
             Me.label1.AutoSize = True
             Me.label1.Location = New System.Drawing.Point(13, 13)
             Me.label1.Name = "label1"
             Me.label1.Size = New System.Drawing.Size(87, 13)
             Me.label1.TabIndex = 2
             Me.label1.Text = "Selected Column"
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(743, 501)
             Me.Controls.Add(Me.label1)
@@ -71,17 +70,16 @@ Namespace Q184421
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
             Me.PerformLayout()
-
         End Sub
 
 #End Region
-
         Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private WithEvents comboBox1 As System.Windows.Forms.ComboBox
-		Private label1 As System.Windows.Forms.Label
-	End Class
-End Namespace
 
+        Private comboBox1 As System.Windows.Forms.ComboBox
+
+        Private label1 As System.Windows.Forms.Label
+    End Class
+End Namespace
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,74 +1,75 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraPivotGrid
 
 Namespace Q184421
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
 
-			Dim ds As List(Of DataObject) = New List(Of DataObject)()
-			For m As Integer = 1 To 12
-				For p As Integer = 1 To 9
-					ds.Add(New DataObject(m, p * m, "Product " & p.ToString()))
-				Next p
-			Next m
-			pivotGridControl1.Fields.AddDataSourceColumn("Month", PivotArea.ColumnArea)
-			pivotGridControl1.Fields.AddDataSourceColumn("Sales", PivotArea.DataArea)
-			pivotGridControl1.Fields.AddDataSourceColumn("ProductName", PivotArea.RowArea)
-			pivotGridControl1.DataSource = ds
+    Public Partial Class Form1
+        Inherits Form
 
-			comboBox1.Items.Clear()
-			For i As Integer = 0 To pivotGridControl1.Cells.ColumnCount - 1
-				comboBox1.Items.Add((i + 1).ToString())
-			Next i
-			comboBox1.SelectedIndex = 9
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            Dim ds As List(Of DataObject) = New List(Of DataObject)()
+            For m As Integer = 1 To 12
+                For p As Integer = 1 To 10 - 1
+                    ds.Add(New DataObject(m, p * m, "Product " & p.ToString()))
+                Next
+            Next
 
-		Private Sub SelectColumn(ByVal columnIndex As Integer)
-			pivotGridControl1.Cells.FocusedCell = New Point(columnIndex, 0)
-			pivotGridControl1.Cells.Selection = New Rectangle(columnIndex, 0, 1, pivotGridControl1.Cells.RowCount)
-		End Sub
+            pivotGridControl1.Fields.AddDataSourceColumn("Month", PivotArea.ColumnArea)
+            pivotGridControl1.Fields.AddDataSourceColumn("Sales", PivotArea.DataArea)
+            pivotGridControl1.Fields.AddDataSourceColumn("ProductName", PivotArea.RowArea)
+            pivotGridControl1.DataSource = ds
+            comboBox1.Items.Clear()
+            For i As Integer = 0 To pivotGridControl1.Cells.ColumnCount - 1
+                comboBox1.Items.Add((i + 1).ToString())
+            Next
 
-		Private Sub comboBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles comboBox1.SelectedIndexChanged
-			If comboBox1.SelectedIndex < 0 Then
-				Return
-			End If
-			SelectColumn(comboBox1.SelectedIndex)
-		End Sub
-	End Class
+            comboBox1.SelectedIndex = 9
+        End Sub
 
-	Public Class DataObject
-		Private month_Renamed, sales_Renamed As Integer
-		Private productName_Renamed As String
+        Private Sub SelectColumn(ByVal columnIndex As Integer)
+            pivotGridControl1.Cells.FocusedCell = New Point(columnIndex, 0)
+            pivotGridControl1.Cells.Selection = New Rectangle(columnIndex, 0, 1, pivotGridControl1.Cells.RowCount)
+        End Sub
 
-		Public Sub New(ByVal month As Integer, ByVal sales As Integer, ByVal productName As String)
-			Me.month_Renamed = month
-			Me.sales_Renamed = sales
-			Me.productName_Renamed = productName
-		End Sub
+        Private Sub comboBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs)
+            If comboBox1.SelectedIndex < 0 Then Return
+            SelectColumn(comboBox1.SelectedIndex)
+        End Sub
+    End Class
 
-		Public ReadOnly Property Month() As Integer
-			Get
-				Return month_Renamed
-			End Get
-		End Property
-		Public ReadOnly Property Sales() As Integer
-			Get
-				Return sales_Renamed
-			End Get
-		End Property
-		Public ReadOnly Property ProductName() As String
-			Get
-				Return productName_Renamed
-			End Get
-		End Property
-	End Class
+    Public Class DataObject
+
+        Private monthField, salesField As Integer
+
+        Private productNameField As String
+
+        Public Sub New(ByVal month As Integer, ByVal sales As Integer, ByVal productName As String)
+            monthField = month
+            salesField = sales
+            productNameField = productName
+        End Sub
+
+        Public ReadOnly Property Month As Integer
+            Get
+                Return monthField
+            End Get
+        End Property
+
+        Public ReadOnly Property Sales As Integer
+            Get
+                Return salesField
+            End Get
+        End Property
+
+        Public ReadOnly Property ProductName As String
+            Get
+                Return productNameField
+            End Get
+        End Property
+    End Class
 End Namespace
